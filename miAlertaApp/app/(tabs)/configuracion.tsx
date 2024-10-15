0d0b8ec6d998d013b38fd745e5b8bf7d23e464b7
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-15 09:55:54 -0300

    feat: cree las vistas de configuracion y contacto

--- miAlertaApp/app/(tabs)/configuracion.tsx
+++ miAlertaApp/app/(tabs)/configuracion.tsx
@@ -7,15 +7,15 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function TabTwoScreen() {
+export default function TabThreeScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Configuración</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>Esta es la pagina de configuracion</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
