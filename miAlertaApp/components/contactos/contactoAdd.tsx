94051c2cd71b43a4c502c468f8506eee0d910b5b
Milagros Soberon <msoberon@alu.frlp.utn.edu.ar>
2024-10-17 16:58:45 -0300

    feat: ajuste el agregar para usarse dentro del modal

--- miAlertaApp/components/contactos/contactoAdd.tsx
+++ miAlertaApp/components/contactos/contactoAdd.tsx
@@ -8,20 +8,17 @@ interface AgregarContactoProps {
 const AgregarContacto: React.FC<AgregarContactoProps> = ({ agregarContacto }) => {
   const [nombre, setNombre] = useState<string>('');
   const [telefono, setTelefono] = useState<string>('');
-  const [isAdding, setIsAdding] = useState<boolean>(false);
 
   const handleAgregarContacto = () => {
     if (nombre && telefono){
     agregarContacto(nombre, telefono);
     setNombre('');
     setTelefono('');
-    setIsAdding(false); //oculta formulario
   }
   };
 
   return (
     <View style={styles.container}>
-      {isAdding ? (
         <>
           <TextInput
             style={styles.input}
@@ -37,12 +34,8 @@ const AgregarContacto: React.FC<AgregarContactoProps> = ({ agregarContacto }) =>
             onChangeText={setTelefono}
             placeholderTextColor="#A9A9A9"
           />
-          <Button title="Agregar Contacto" onPress={handleAgregarContacto} color="#4fdf57"/>
-          <Button title="Cancelar" onPress={() => setIsAdding(false)} color="#f24022" />
+          <Button title="Agregar" onPress={handleAgregarContacto} color="#4fdf57"/>
         </>
-      ) : (
-        <Button title="Agregar Contacto" onPress={() => setIsAdding(true)} color="#4fdf57" />
-      )}
     </View>
   );
 };
@@ -55,9 +48,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
     backgroundColor: '#fff',
+    width: 300,
     color: '#000',
     padding: 10,
-    marginBottom: 10,
+    marginBottom: 20,
+    marginTop: 5,
   },
 });
 
