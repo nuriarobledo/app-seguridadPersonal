86ae5f5e051eb7974660ba782948fa75fd0a27a7
Gonzalo Baez <gezbaez@gmail.com>
2024-10-18 00:06:27 -0300

    feat: agrego mapa a la navegacion, falta probar

--- miAlertaApp/app/(tabs)/mapa.tsx
+++ miAlertaApp/app/(tabs)/mapa.tsx
@@ -1,21 +1,60 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
-
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import React, { useState, useEffect } from 'react';
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';  // Importa expo-location
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function TabFourScreen() {
+// Define el tipo para la región
+interface IRegion {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
+
+export default function Lugares_Seguros() {
+  const [region, setRegion] = useState<IRegion | null>(null);  // Estado con tipo para la región
+
+  useEffect(() => {
+    (async () => {
+      // Pedir permisos de ubicación al iniciar
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Permiso de ubicación denegado.");
+        return;
+      }
+
+      // Obtener la ubicación actual
+      let location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+
+      // Establecer la región con la ubicación actual
+      setRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.01,  // Zoom del mapa
+        longitudeDelta: 0.01, // Zoom del mapa
+      });
+    })();
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">MAPA</ThemedText>
+      <ThemedView style={styles.container}>
+        {region ? (
+          <MapView
+            style={StyleSheet.absoluteFill}
+            provider={PROVIDER_GOOGLE}
+            region={region}
+            showsUserLocation={true}
+        />
+      ) : null}  {/* Muestra el mapa solo cuando la región esté definida */}
       </ThemedView>
-      <ThemedText>Esta es la pagina de mapa</ThemedText>
     </ParallaxScrollView>
   );
 }
@@ -31,4 +70,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: 8,
   },
-});
+  container: {
+    flex: 1,
+  },
+})
