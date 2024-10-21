9f4222d024963f8d719cf776ef2bba12a922703a
Gonzalo Baez <gezbaez@gmail.com>
2024-10-21 01:06:31 -0300

    feat: correcciones primera vista del mapa

--- miAlertaApp/app/(tabs)/mapa.tsx
+++ miAlertaApp/app/(tabs)/mapa.tsx
@@ -1,76 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator, Text, TouchableOpacityBase, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';  // Importa expo-location
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { useNavigation } from 'expo-router';
 
-// Define el tipo para la región
-interface IRegion {
-  latitude: number;
-  longitude: number;
-  latitudeDelta: number;
-  longitudeDelta: number;
-}
+const INITIAL_REGION={
+  latitude: -34.90486,
+  longitude: -57.92606,
+  latitudeDelta: 2,
+  longitudeDelta: 2,
+};
 
-export default function Lugares_Seguros() {
-  const [region, setRegion] = useState<IRegion | null>(null);  // Estado con tipo para la región
+export default function mapa(){
+  const mapRef = useRef<MapView | null>(null);
+  const navigation =  useNavigation();
 
-  useEffect(() => {
-    (async () => {
-      // Pedir permisos de ubicación al iniciar
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log("Permiso de ubicación denegado.");
-        return;
-      }
-
-      // Obtener la ubicación actual
-      let location = await Location.getCurrentPositionAsync({});
-      const { latitude, longitude } = location.coords;
-
-      // Establecer la región con la ubicación actual
-      setRegion({
-        latitude,
-        longitude,
-        latitudeDelta: 0.01,  // Zoom del mapa
-        longitudeDelta: 0.01, // Zoom del mapa
-      });
-    })();
+  useEffect(() =>{
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={focusMap}>
+          <View style={{ padding: 10}}>
+            <Text> Focus </Text>
+          </View>
+        </TouchableOpacity>
+      ),
+    });
   }, []);
 
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
-      <ThemedView style={styles.container}>
-        {region ? (
-          <MapView
-            style={StyleSheet.absoluteFill}
-            provider={PROVIDER_GOOGLE}
-            region={region}
-            showsUserLocation={true}
-        />
-      ) : null}  {/* Muestra el mapa solo cuando la región esté definida */}
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+  const focusMap =() => {
+    const UTN = {
+      latitude: -34.90486,
+      longitude: -58.3816,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
 
-const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    gap: 8,
-  },
-  container: {
-    flex: 1,
-  },
-})
+    mapRef.current?.animateCamera({ center: UTN, zoom:100}, {duration: 3000});
+  };
+
+  return(
+    <View style={{flex: 1}}>
+      <MapView
+        style={StyleSheet.absoluteFill}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        showsMyLocationButton
+        ref={mapRef}
+      />
+    </View>
+    );
+
+}
