93d0da4cca9124a608c4d0b5930e718c10db8c20
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-23 17:15:46 -0300

    feat: cambie nombre de index

--- miAlertaApp/app/(tabs)/_layout.tsx
+++ miAlertaApp/app/(tabs)/_layout.tsx
@@ -16,7 +16,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
