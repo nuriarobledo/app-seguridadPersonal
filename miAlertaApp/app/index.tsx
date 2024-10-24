0b2394b1dc4706af7056f2654b570cb31de26311
Nuria Robledo <nuriarobledo0@gmail.com>
2024-10-24 16:00:24 -0300

    feat: mejore el tipo y contraseña

--- miAlertaApp/app/index.tsx
+++ miAlertaApp/app/index.tsx
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, Alert, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../assets/types'; 
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../assets/types";
 
-type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'index'>;
+type LoginScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "index"
+>;
 
 const Login = () => {
   const navigation = useNavigation<LoginScreenNavigationProp>();
-  const [email, setEmail] = useState('');
-  const [pin, setPin] = useState('');
+  const [email, setEmail] = useState("");
+  const [pin, setPin] = useState("");
 
   const handleLogin = () => {
     // Credenciales hardcodeadas
-    const hardcodedEmail = 'usuario@example.com';
-    const hardcodedPin = '1234';
+    const hardcodedEmail = "usuario@example.com";
+    const hardcodedPin = "1234";
 
     if (email === hardcodedEmail && pin === hardcodedPin) {
       // Navegar a la pantalla de inicio
-      navigation.navigate('(tabs)'); 
+      navigation.navigate("(tabs)");
     } else {
       // Mostrar un mensaje de error
-      Alert.alert('Error', 'Usuario o pin incorrectos');
+      Alert.alert("Error", "Usuario o pin incorrectos");
     }
   };
 
   const handleRegistro = () => {
-
-      navigation.navigate('registro'); 
-   
+    navigation.navigate("registro");
   };
 
   return (
@@ -39,13 +48,15 @@ const Login = () => {
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
+        keyboardType="email-address"
       />
       <TextInput
         style={styles.input}
         placeholder="PIN"
-        keyboardType="phone-pad"
         value={pin}
         onChangeText={setPin}
+        secureTextEntry
+        keyboardType="numeric"
       />
       <TouchableOpacity style={styles.buttonLogin} onPress={handleLogin}>
         <Text style={styles.buttonText}>Iniciar Sesión</Text>
@@ -62,39 +73,39 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 16,
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 12,
     paddingLeft: 8,
   },
   buttonLogin: {
-    backgroundColor: '#007BFF', //celeste
-    paddingVertical: 12, 
-    paddingHorizontal: 20, 
-    borderRadius: 5, 
-    alignItems: 'center', 
-    marginTop: 10, 
+    backgroundColor: "#007BFF", //celeste
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 10,
   },
   buttonText: {
-    color: '#FFFFFF', // blanco
-    fontSize: 16, 
-    fontWeight: 'bold', 
+    color: "#FFFFFF", // blanco
+    fontSize: 16,
+    fontWeight: "bold",
   },
   buttonRegistro: {
-    color: '#003366', // Azul oscuro
-    textDecorationLine: 'underline',
-    textAlign: 'center',
-    marginTop: 20, 
+    color: "#003366", // Azul oscuro
+    textDecorationLine: "underline",
+    textAlign: "center",
+    marginTop: 20,
   },
 });
 
--- miAlertaApp/app/registro.tsx
+++ miAlertaApp/app/registro.tsx
@@ -1,85 +1,91 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../assets/types'; 
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../assets/types";
 
-type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'index'>;
+type LoginScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "index"
+>;
 
 const Registro = () => {
-    const navigation = useNavigation<LoginScreenNavigationProp>();
-    const [nombre, setNombre] = useState('');
-    const [email, setEmail] = useState('');
-    const [numeroCelular, setNumeroCelular] = useState('');
-    const [pin, setPin] = useState('');
+  const navigation = useNavigation<LoginScreenNavigationProp>();
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [numeroCelular, setNumeroCelular] = useState("");
+  const [pin, setPin] = useState("");
 
-    const handleRegistro = () => {
-        Alert.alert('Registro exitoso', `Nombre: ${nombre}\nEmail: ${email}\nNúmero Celular: ${numeroCelular}\nCódigo PIN: ${pin}`);
-
-        // espera 2 segundos antes de navegar
-        setTimeout(() => {
-            navigation.navigate('index'); 
-        }, 800); // 2000 ms = 2 segundos
-    };
-
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Registro</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Nombre"
-                value={nombre}
-                onChangeText={setNombre}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Número Celular"
-                value={numeroCelular}
-                onChangeText={setNumeroCelular}
-                keyboardType="phone-pad"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Código PIN"
-                value={pin}
-                onChangeText={setPin}
-                keyboardType="phone-pad"
-            />
-            <Button title="Registrarme" onPress={handleRegistro} />
-        </View>
+  const handleRegistro = () => {
+    Alert.alert(
+      "Registro exitoso",
+      `Nombre: ${nombre}\nEmail: ${email}\nNúmero Celular: ${numeroCelular}\nCódigo PIN: ${pin}`
     );
+
+    // espera 2 segundos antes de navegar
+    setTimeout(() => {
+      navigation.navigate("index");
+    }, 800); // 2000 ms = 2 segundos
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Registro</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Número Celular"
+        value={numeroCelular}
+        onChangeText={setNumeroCelular}
+        keyboardType="phone-pad"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Código PIN"
+        value={pin}
+        onChangeText={setPin}
+        secureTextEntry
+        keyboardType="numeric"
+      />
+      <Button title="Registrarme" onPress={handleRegistro} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-        padding: 16,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    input: {
-        height: 40,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 5,
-        marginBottom: 15,
-        paddingHorizontal: 10,
-        width: '100%',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    width: "100%",
+  },
 });
 
 export default Registro;
