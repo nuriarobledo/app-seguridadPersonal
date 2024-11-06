726f355dc9c7e5b012c0aaeaf49248102ad5a7a2
Nuria Robledo <nuriarobledo0@gmail.com>
2024-11-05 23:32:29 -0300

    feat: creo la bd

--- miAlertaApp/database/database.ts
+++ miAlertaApp/database/database.ts
@@ -1,4 +1,5 @@
 import * as SQLite from "expo-sqlite";
+import * as FileSystem from "expo-file-system";
 
 const db = SQLite.openDatabaseAsync("db_seguridadPersonal.db");
 
@@ -7,7 +8,7 @@ export const initializeDatabase = async () => {
 
   // Eliminar la bd
   // try {
-  //   // await database.execAsync(`DROP TABLE IF EXISTS Usuario;`);
+    // await database.execAsync(`DROP TABLE IF EXISTS Usuario;`);
   //   // await database.execAsync(`DROP TABLE IF EXISTS UbicacionCompartida;`);
   //   await database.execAsync(`DROP TABLE IF EXISTS ContactoEmergencia;`);
   //   //await database.execAsync(`DROP TABLE IF EXISTS Alerta;`);
@@ -19,74 +20,78 @@ export const initializeDatabase = async () => {
   //   console.error('Error al eliminar las tablas:', error);
   // }
   // };
+  try {
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS Usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        email TEXT NOT NULL,
+        celular INTEGER NOT NULL,
+        pinHash TEXT NOT NULL,
+        firebaseId TEXT
+      );`
+    );
 
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS Usuario (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      nombre TEXT NOT NULL,
-      email TEXT NOT NULL,
-      celular INTEGER NOT NULL,
-      codigoPin INTEGER NOT NULL,
-      firebaseId TEXT,  
-    );`
-  );
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS ContactoEmergencia (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          idUsuario INTEGER NOT NULL,
+          nombre TEXT NOT NULL,
+          celular INTEGER NOT NULL,
+          relacion TEXT NOT NULL,
+          FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+          );`
+    );
 
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS UbicacionCompartida (
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS UbicacionCompartida (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        idUsuario INTEGER NOT NULL,
+        idContactoEmergencia INTEGER NOT NULL,
+        tiempoInicio DATETIME NOT NULL,
+        tiempoFin DATETIME,
+        ubicacion TEXT NOT NULL,
+        FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+        FOREIGN KEY (idContactoEmergencia) REFERENCES ContactoEmergencia(id)
+      );`
+    );
+   
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS Alerta (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          idUsuario INTEGER NOT NULL,
+          idUbicacionCompartida INTEGER NOT NULL,
+          mensaje TEXT NOT NULL,
+          FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+          FOREIGN KEY (idUbicacionCompartida) REFERENCES UbicacionCompartida(id)
+          );`
+    );
+
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS AlertaContacto (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          idAlerta INTEGER NOT NULL,
+          idContactoEmergencia INTEGER NOT NULL,
+          FOREIGN KEY (idAlerta) REFERENCES Alerta(id),
+          FOREIGN KEY (idContactoEmergencia) REFERENCES ContactoEmergencia(id)
+          );`
+    );
+
+    await database.execAsync(
+      `CREATE TABLE IF NOT EXISTS IntentoAutenticacion (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       idUsuario INTEGER NOT NULL,
-      idContactoEmergencia INTEGER NOT NULL,
-      tiempoInicio DATETIME NOT NULL,
-      tiempoFin DATETIME,
-      ubicacion TEXT NOT NULL,
-      FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
-      FOREIGN KEY (idContactoEmergencia) REFERENCES ContactoEmergencia(id)
-    );`
-  );
-
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS ContactoEmergencia (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        idUsuario INTEGER NOT NULL,
-        nombre TEXT NOT NULL,
-        celular INTEGER NOT NULL,
-        relacion TEXT NOT NULL,
-        FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
-        );`
-  );
-
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS Alerta (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        idUsuario INTEGER NOT NULL,
-        idUbicacionCompartida INTEGER NOT NULL,
-        mensaje TEXT NOT NULL,
-        FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
-        FOREIGN KEY (idUbicacionCompartida) REFERENCES UbicacionCompartida(id)
-        );`
-  );
-
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS AlertaContacto (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        idAlerta INTEGER NOT NULL,
-        idContactoEmergencia INTEGER NOT NULL,
-        FOREIGN KEY (idAlerta) REFERENCES Alerta(id),
-        FOREIGN KEY (idContactoEmergencia) REFERENCES ContactoEmergencia(id)
-        );`
-  );
-
-  await database.execAsync(
-    `CREATE TABLE IF NOT EXISTS IntentoAutenticacion (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    idUsuario INTEGER NOT NULL,
-    tiempoIntento DATETIME NOT NULL,
-    valido BOOLEAN NOT NULL,
-    FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
-    );`
-  );
+      tiempoIntento DATETIME NOT NULL,
+      valido BOOLEAN NOT NULL,
+      FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+      );`
+    );
+    console.log("Todas las tablas han sido creadas con éxito");
+  } catch (error) {
+    console.error("Error al crear las tablas:", error);
+  }
 };
-
+initializeDatabase();
 //---------------- FUNCIONES -----------------
 // Obtener todos los usuarios
 //Usuarios
@@ -112,7 +117,7 @@ export const getUsuarios = async (): Promise<Usuario[]> => {
   }
 };
 
-export const addUsuarios = async (
+export const addUsuario = async (
   nombre: string,
   email: string,
   celular: number,
