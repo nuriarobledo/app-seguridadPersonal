e41a33cb38e14fea937241a17eca6516c83266ec
Nuria Robledo <nuriarobledo0@gmail.com>
2024-11-16 19:10:43 -0300

    feat: agrego nombre a rutas

--- miAlertaApp/assets/types.ts
+++ miAlertaApp/assets/types.ts
@@ -2,5 +2,8 @@ export type RootStackParamList = {
   index: undefined; // Pantalla de inicio de sesión
   registro: undefined; // Pantalla de registro
   '(tabs)': undefined; // Pantalla principal
+  funciones: undefined; // Pantalla de funciones
+  mapa: undefined; // Pantalla del mapa
+  consejosSeguridad: undefined; // Pantalla de consejos de seguridad
   '+not-found': undefined; // Pantalla no encontrada
 };
