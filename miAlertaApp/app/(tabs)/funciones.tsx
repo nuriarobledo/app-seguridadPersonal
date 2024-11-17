5ccd624aba85de539363e5db7abb26d90b873b99
Nuria Robledo <nuriarobledo0@gmail.com>
2024-11-16 23:17:39 -0300

    feat: agrego navegacion a llamada

--- miAlertaApp/app/(tabs)/funciones.tsx
+++ miAlertaApp/app/(tabs)/funciones.tsx
@@ -1,17 +1,17 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, TouchableOpacity } from 'react-native';
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Image, Platform, TouchableOpacity } from "react-native";
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackParamList } from "../../assets/types"; 
+import { RootStackParamList } from "../../assets/types";
 
 type ScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
-  "funciones" 
+  "funciones"
 >;
 
 export default function FuncionesScreen() {
@@ -19,53 +19,62 @@ export default function FuncionesScreen() {
 
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
     >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Funciones</ThemedText>
       </ThemedView>
 
       {/* Botón para navegar a mapa.tsx */}
-      <TouchableOpacity 
-        style={styles.button} 
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("mapa")}
       >
         <ThemedText style={styles.buttonText}>Mapa</ThemedText>
       </TouchableOpacity>
 
-       {/* Botón para navegar a consejosSeguridad.tsx */}
-       <TouchableOpacity 
-        style={styles.button} 
+      {/* Botón para navegar a consejosSeguridad.tsx */}
+      <TouchableOpacity
+        style={styles.button}
         onPress={() => navigation.navigate("consejosSeguridad")}
       >
         <ThemedText style={styles.buttonText}>Consejos de seguridad</ThemedText>
       </TouchableOpacity>
 
+      {/* Botón para navegar a llamadaFalsa.tsx */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("llamadaFalsa")}
+      >
+        <ThemedText style={styles.buttonText}>Llamada Falsa</ThemedText>
+      </TouchableOpacity>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   headerImage: {
-    color: '#808080',
+    color: "#808080",
     bottom: -90,
     left: -35,
-    position: 'absolute',
+    position: "absolute",
   },
   titleContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
   },
   button: {
-    backgroundColor: '#007BFF', // Color del botón
+    backgroundColor: "#007BFF", 
     padding: 10,
     borderRadius: 5,
-    alignItems: 'center',
-    marginVertical: 10, // Espaciado vertical
+    alignItems: "center",
+    marginVertical: 10, 
   },
   buttonText: {
-    color: '#FFFFFF', // Color del texto del botón
+    color: "#FFFFFF", 
     fontSize: 16,
   },
 });
